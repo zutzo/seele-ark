ae652097ea8bb598ca1bcba77835d0fad7d287a3
Seele <126886178+zutzo@users.noreply.github.com>
2026-07-23 03:34:50 +0000

    Update assets

---
+++ ui/assets/singbox-B4bBm8h7.js
@@ -0,0 +1,1 @@
+import{aT as e,aU as a}from"./index-DJSEHHoy.js";const s=async()=>{e.value=[],a.value=[]};export{s as fetchRules};
--- ui/assets/singbox-D2GFF1TC.js
+++
@@ -1,1 +0,0 @@
-import{aT as e,aU as a}from"./index-CP7y7lZb.js";const s=async()=>{e.value=[],a.value=[]};export{s as fetchRules};
